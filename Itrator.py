914386dde7686a17888b51b4693cb00320f9334c
Chandan Mondal <100483503+Chandan1707@users.noreply.github.com>
2022-02-27 13:45:46 +0530

    Itrator class and Object define

--- Itrator.py
+++ Itrator.py
@@ -1,1 +1,28 @@
 
+class PowTwo:
+    def __init__(self, max):
+        self.max = max
+        self.num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num > self.max:
+            raise StopIteration
+        val = self.num
+        self.num += 1
+        return val*val
+
+
+c1 = PowTwo(10)
+it = iter(c1)
+c2 = PowTwo(12)
+while True:
+    try:
+        print(c1.__next__())
+    except StopIteration:
+        break
+
+for i in c2:
+    print(i)
